6007f8ff4c35c89755406676a98f77fe51bf27a8
LiteTekno92 <lite.tekno92@gmail.com>
2019-04-09 10:27:14 +0200

    drawer moved

---
+++ lib/drawer.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+
+class Drawer extends StatelessWidget {
+  //final appTitle = 'Drawer Demo';
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Center(child: 
+       ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,41 @@ class TabbedAppBarSample extends StatelessWidget {
       home: DefaultTabController(
         length: choices.length,
         child: Scaffold(
+          drawer: Drawer(
+            // Add a ListView to the drawer. This ensures the user can scroll
+            // through the options in the Drawer if there isn't enough vertical
+            // space to fit everything.
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  child: Text('Drawer Header'),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                ),
+                ListTile(
+                  title: Text('Item 1'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('Item 2'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
           appBar: AppBar(
             title: const Text('Tabbed AppBar'),
             bottom: TabBar(
